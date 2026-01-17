7d5dabab7d6a653c0283ee137f5f380437fb5797
CiscoStev42 <sjstone91@tutanota.com>
2026-01-17 16:08:02 -0600

    Added in-progress work project to help find log files easier

---
+++ dir.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Enter a SN: "
+read SN
+
+echo "Press 1 to Select the Unit's Directory."
+echo "Or Press 2 to Search the Entire System from Root."
+read choice
+
+if [ choice == 1 ]; then
+  echo "What type of unit is this? (Case-Sensitive)"
+  read unit
+  cd /TSC/TSC/Naomi/${unit}/log && ll -tr | grep ${SN}
+elif [ choice == 2 ]; then
+  find / -name "*${SN}*" 2>/dev/null 
+fi
+
+
