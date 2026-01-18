bbd5c8ca0550a321feb7f3aa631b0e3b7a06c7b9
CiscoSteve42 <skoomasteve42@proton.me>
2026-01-18 04:39:56 -0600

    minor syntax fixes

--- dir.sh
+++ dir.sh
@@ -7,11 +7,11 @@ echo "Press 1 to Select the Unit's Directory."
 echo "Or Press 2 to Search the Entire System from Root."
 read choice
 
-if [ choice == 1 ]; then
+if [ choice == "1" ]; then
   echo "What type of unit is this? (Case-Sensitive)"
   read unit
   cd /TSC/TSC/Naomi/${unit}/log && ll -tr | grep ${SN}
-elif [ choice == 2 ]; then
+elif [ choice == "2" ]; then
   find / -name "*${SN}*" 2>/dev/null 
 fi
 
